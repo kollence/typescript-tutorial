d34bc9ee7ee9743da0cc0ebadceaa1fc96081bf4
kole <misterija@gmail.com>
2023-08-05 15:41:44 +0200

    Looking corect way of using types without errors

--- lesson1/src/main.ts
+++ lesson1/src/main.ts
@@ -9,7 +9,12 @@ isOk = false;
 album = 5150;
 
 
-const sum = (a: string, b: number) => {
+const sum = (a: string, b: number) => a + b;
+const sum2 = (a: number, b: number) : number => a + b;
 
-    return a + b;
-}
+let uniType: string | number;
+uniType = 'string';
+let uniType2: string | number | boolean;
+uniType2 = true;
+let uniType3: string | number | boolean = true;
+uniType3 = false;
