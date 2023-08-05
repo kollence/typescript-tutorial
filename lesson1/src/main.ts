a12664993cf141334575458ca892adb2ad4849a8
kole <misterija@gmail.com>
2023-08-05 15:36:13 +0200

    first commit

---
+++ lesson1/src/main.ts
@@ -0,0 +1,15 @@
+let myname: string = "TypeScript";
+let age: number = 10;
+let isOk: boolean = true;
+let album: any;
+
+myname = 'JavaScript';
+age = 11;
+isOk = false;
+album = 5150;
+
+
+const sum = (a: string, b: number) => {
+
+    return a + b;
+}
