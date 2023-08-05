c1bd1daa34cd5fe4e77136708503892a6a214429
kole <misterija@gmail.com>
2023-08-05 16:34:38 +0200

    let var to compare:works-const var compare:error

--- lesson2/build/js/main.js
+++ lesson2/build/js/main.js
@@ -1,3 +1,42 @@
 "use strict";
-let stringType = 'nuumm';
-console.log(stringType);
+let stringArray = ['a', 'b', 'c'];
+let guitars = ['Gibson', 'Fender', 'PRS', 5150];
+let mixData = ['Gibson', 1, true];
+stringArray[0] = 'd';
+stringArray.push('e');
+guitars[0] = 1984;
+guitars.push('Ibanez');
+mixData[2] = 1984;
+mixData[0] = 'Ibanez';
+mixData[1] = false;
+let emptyArray = [];
+let bands = [];
+bands.push('Van Halen');
+// Tuple
+let tupleType = ['Gibson', 1984, true];
+let mixed = ['Gibson', 1984, true];
+// mixed = tupleType // works
+// tupleType = mixed // error
+tupleType[2] = false;
+tupleType[0] = 'Ibanez'; // works
+// Object
+let person;
+person = [];
+console.log(typeof person);
+person = bands;
+person = {};
+const exampleObject = { name: 'Van Halen', age: 40, isBand: true };
+exampleObject.name = 'Van Halen II';
+let person2 = {
+    name: 'Van Halen',
+    age: 40,
+    isBand: true,
+    albums: ['Van Halen', 'Van Halen II', 1984]
+};
+let JP = {
+    name: 'Jimmy',
+    age: 45,
+    isBand: true,
+    albums: ['I', 'II', 1989]
+};
+person2 = JP;
--- lesson2/src/main.ts
+++ lesson2/src/main.ts
@@ -1,3 +1,61 @@
-let stringType: string = 'nuumm';
+let stringArray = ['a', 'b', 'c'];
 
-console.log(stringType);
+let guitars = ['Gibson', 'Fender', 'PRS', 5150];
+
+let mixData = ['Gibson', 1, true];
+
+stringArray[0] = 'd';
+stringArray.push('e');
+
+guitars[0] = 1984;
+guitars.push('Ibanez');
+
+mixData[2] = 1984;
+mixData[0] = 'Ibanez';
+mixData[1] = false;
+
+let emptyArray = [];
+let bands: string[] = [];
+bands.push('Van Halen');
+
+// Tuple
+let tupleType: [string, number, boolean] = ['Gibson', 1984, true];
+let mixed = ['Gibson', 1984, true];
+
+// mixed = tupleType // works
+// tupleType = mixed // error
+tupleType[2] = false; 
+tupleType[0] = 'Ibanez'; // works
+
+// Object
+let person: object
+person = []
+console.log(typeof person)
+person = bands
+person = {}
+
+const exampleObject = { name: 'Van Halen', age: 40, isBand: true }
+exampleObject.name = 'Van Halen II'
+// exampleObject.age = true    // error
+
+type Person = {
+    name: string,
+    age: number,
+    isBand: boolean,
+    albums: (string | number)[]
+}
+
+let person2: Person = {   // works
+    name: 'Van Halen',
+    age: 40,
+    isBand: true,
+    albums: ['Van Halen', 'Van Halen II', 1984]
+}
+let JP: Person = {   // works
+    name: 'Jimmy',
+    age: 45,
+    isBand: true,
+    albums: ['I', 'II', 1989]
+}
+
+person2 = JP 
